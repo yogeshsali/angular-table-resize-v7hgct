f6501b2957c16b86a32f0095c72edb57793c5b4b
yogeshsali <er.yogeshsali@gmail.com>
2022-07-15 11:54:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-table-resize-v7hgct
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-table-resize-v7hgct)
---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import './polyfills';
+
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './app/material-module';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+
+import {ResizableTableComponent} from './app/resizable-table.component';
+import {ResizeColumnDirective} from './app/resize-column.directive';
+
+// Default MatFormField appearance to 'fill' as that is the new recommended approach and the
+// `legacy` and `standard` appearances are scheduled for deprecation in version 10.
+// This makes the examples that use MatFormField render the same in StackBlitz as on the docs site.
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  entryComponents: [ResizableTableComponent],
+  declarations: [ResizableTableComponent, ResizeColumnDirective],
+  bootstrap: [ResizableTableComponent],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ]
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
